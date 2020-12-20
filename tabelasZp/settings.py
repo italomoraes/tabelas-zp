a60549c9fc929173c87931112c530da898900576
Diogo Bernini Milagres <diogoberninimilagres@MacBook-Pro-de-Diogo.local>
2020-12-20 16:41:42 -0300

    resolve static

--- tabelasZp/settings.py
+++ tabelasZp/settings.py
@@ -119,7 +119,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = ''
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = ( os.path.join('static'), )
+print(STATIC_ROOT)
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
