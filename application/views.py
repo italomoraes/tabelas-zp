16305b2290c888daa29dcf99f78bcb38e85385ba
Italo Moraes <italo_kz@hotmail.com>
2020-12-20 16:54:03 -0300

    População do DB e empreendimentos e casas retornando na view index

--- application/views.py
+++ application/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from application.models import *
 
 # Create your views here.
 
 def post_list(request):
-    return render(request, 'index.html', {})
+	empreendimentos = Empreendimentos.objects.all()
+	casas = Casa.objects.all()
+	return render(request, 'index.html', {'empreendimentos': empreendimentos, 'casas': casas})
 
 def empreendimento_detalhes(request):
     return render(request, 'tabelas.html', {})
