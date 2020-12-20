33bcb004cfc771de5d1090c10e89707f3842be9b
Italo Moraes <italo_kz@hotmail.com>
2020-12-20 16:15:23 -0300

    Mudança do header do site de administração

--- application/admin.py
+++ application/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from application.models import *
 
-# Register your models here.
+admin.site.site_header = 'Painel do corretor'
 
+# Register your models here.
 admin.site.register(Empreendimento)
 admin.site.register(Unidade)
 admin.site.register(MidiaEmpreendimento)
 admin.site.register(Casa)
 admin.site.register(MidiaCasa)
+
